509f318cb771ec9224e10ee514a20ee5c08f9590
Felipe Lima <felipesoueu@live.com>
2018-06-29 23:43:02 -0300

    Teste para aloca arvore criado

--- include/arv_decisoes.h
+++ include/arv_decisoes.h
@@ -2,16 +2,7 @@
 #define _ARV_DECISOES_
 
 #include "../include/tabuleiro.h"
-
-/* Definição da estrutura da árvore de decisões */
-/*! \struct Tree
-		\brief Estrutura da árvore.
-
-		Estrutura que armazena uma referência para a raíz da árvore de decisões
-*/
-typedef struct Tree{
-	NodeTree* root; /*!< Ponteiro para uma variável do tipo ponteiro que representa a raiz da árvore */
-}Tree;
+#include "../include/logica.h"
 
 /* Definição da estrutura de um nó da árvore de decisões */
 /*! \struct NodeTree
@@ -23,8 +14,19 @@ typedef struct NodeTree{
 	TBoard* board;	/*!< Ponteiro para uma variável do tipo Tboard que representa a organização do tabuleiro depois de alguma jogada */
 	Move* play;	/*!< Ponteiro para uma variável do tipo Move que representa a jogada que originou a configuração do tabuleiro */
 	int n_child; /*!< Inteiro que armazena o número de filhos que o nó da árvore tem */
-	NoArv ** child; /*!< Vetor de ponteiros para os filhos do nó */
+	NodeTree ** child; /*!< Vetor de ponteiros para os filhos do nó */
 }NodeTree;
 
+/* Definição da estrutura da árvore de decisões */
+/*! \struct Tree
+		\brief Estrutura da árvore.
+
+		Estrutura que armazena uma referência para a raíz da árvore de decisões
+*/
+typedef struct Tree{
+	NodeTree* root; /*!< Ponteiro para uma variável do tipo ponteiro que representa a raiz da árvore */
+}Tree;
+
+Tree* AlocateTree(void);
 
 #endif
