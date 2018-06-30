ec13c3137363395c4f643458353c357905e605ba
Andre Macedo <andremacedo.pv@gmail.com>
2018-06-30 00:00:09 -0300

    Evolução do teste para a ValidBoard analisar rainhas

--- test/TEST_tabuleiro.c
+++ test/TEST_tabuleiro.c
@@ -347,6 +347,54 @@ TEST(Test_Valid_Board, Verify_Two_Kings){
 	EXPECT_EQ(0, ValidBoard(&board));
 }
 
+/* Teste para verificar a função que verifica se um tabuleiro é válido para tabuleiros com mais de 9 rainhas.
+   Procedimentos:
+   -Criar um tabuleiro vazio;
+   -Adicionar os dois reis de cada cor;
+   -Adicionar 9 rainhas de cada cor;
+   -Chamar a função;
+   -Adicionar uma rainha branca e chamar a função;
+   -Remover a última rainha adicionada, adicionar uma preta e chamar a função.
+   Resultados:
+   -É esperado que a função retorne 1, para a primeira chamada e 0 para as outras duas.
+*/
+TEST(Test_Valid_Board, Verify_Queens){
+	TBoard board;
+	StartEmptyBoard(&board);
+
+	InsertPiece(&board, B_KING , 0, 0);
+	InsertPiece(&board, W_KING , 7, 7);
+
+	InsertPiece(&board, W_QUEEN , 7, 6);
+	InsertPiece(&board, W_QUEEN , 7, 5);
+	InsertPiece(&board, W_QUEEN , 7, 4);
+	InsertPiece(&board, W_QUEEN , 7, 3);
+	InsertPiece(&board, W_QUEEN , 7, 2);
+	InsertPiece(&board, W_QUEEN , 7, 1);
+	InsertPiece(&board, W_QUEEN , 7, 0);
+	InsertPiece(&board, W_QUEEN , 6, 7);
+	InsertPiece(&board, W_QUEEN , 6, 6);
+
+	InsertPiece(&board, B_QUEEN , 5, 5);
+	InsertPiece(&board, B_QUEEN , 5, 6);
+	InsertPiece(&board, B_QUEEN , 5, 7);
+	InsertPiece(&board, B_QUEEN , 6, 0);
+	InsertPiece(&board, B_QUEEN , 6, 1);
+	InsertPiece(&board, B_QUEEN , 6, 2);
+	InsertPiece(&board, B_QUEEN , 6, 3);
+	InsertPiece(&board, B_QUEEN , 6, 4);
+	InsertPiece(&board, B_QUEEN , 6, 5);
+
+	EXPECT_EQ(1, ValidBoard(&board));
+
+	InsertPiece(&board, W_QUEEN , 4, 4);
+	EXPECT_EQ(0, ValidBoard(&board));
+
+	RemovePiece(&board, 4, 4);
+	InsertPiece(&board, B_QUEEN , 4, 4);
+	EXPECT_EQ(0, ValidBoard(&board));
+}
+
 int main(int argc, char **argv){
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
