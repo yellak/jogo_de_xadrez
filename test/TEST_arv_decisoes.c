a2d91ce737ae3a2badc72a1d07cd8582571f8a74
Felipe Lima <felipesoueu@live.com>
2018-06-30 00:29:28 -0300

    Comentarios inseridos

--- test/TEST_arv_decisoes.c
+++ test/TEST_arv_decisoes.c
@@ -18,13 +18,24 @@ TEST(Test_AlocateTree, Verify_Alocation_Tree){
 	EXPECT_EQ(1, tree != NULL);
 } 
 
-
+/* Teste para verificar a validade da função que aloca um nó de uma árvore
+   Procedimento:
+   	-Criar Variável para o nó;
+   	-Chamar função que aloca o nó;
+   	-Verificar se o nó retornado pela função é não nulo;
+   	-Verificar se o componente "board" do nó é não nulo;
+   	-Verificar se o componente "n_child" do nó é igual ao esperado(4);
+   Resultados:
+   	-Todas as variáveis devem estar alocadas corretamente
+ */
 TEST(Test_AlocateNodeTree, Verify_Alocation_Node){
 	NodeTree * node;
 	node = AlocateNodeTree(4);
 
 	/* Testar a alocação */
 	EXPECT_EQ(1, node != NULL);
+	EXPECT_EQ(1, node->board != NULL);
+	EXPECT_EQ(4, node->n_child);
 } 
 
 int main(int argc, char **argv){
