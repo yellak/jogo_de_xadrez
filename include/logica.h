dafed7e9dc94d576d74877b6723c16cc6254638e
Eduardo Lemos Rocha <dudulr10@gmail.com>
2018-07-01 15:33:08 -0300

    Undefined Aleatórios

--- include/logica.h
+++ include/logica.h
@@ -1,29 +1,8 @@
 #ifndef LOGICA_H_
 #define LOGICA_H_
 
+#include "../include/list_of_moves.h"
 #include "../include/tabuleiro.h"
-
-/* Definição da estrutura que irá armazenar uma jogada possível a partir de um tabuleiro */
-/*! \struct Move
-		\brief Estrutura que irá caracterizar a jogada.
-
-		Estrutura que armazena uma maneira de interpretar uma jogada.
-*/
-typedef struct Move{
-	int origin[2]; /*!< Array que armazena as coordenadas de origem do movimento. */
-	int destiny[2]; /*!< Array que armazena as coordenadas de destino do movimento. */
-}Move;
-
-/* Definição da estrutura que irá armazenar as jogadas possíveis a partir de um tabuleiro. */
-/*! \struct ListOfMoves
-		\brief Estrutura representará todas as possíveis jogadas de um tabuleiro.
-
-		Array do tipo Move que é importante para a análise das jogadas.
-*/
-typedef struct ListOfMoves{
-	int howmany; /*!< Inteiro que guarda quantas jogadas são possíveis de serem feitas. */
-	Move *Plays; /*!< Array que armazena todas as jogadas possíveis com base em um tabuleiro. */
-}ListOfMoves;
 
 /* Funções para manipulação da lógica do xadrez: */
 
@@ -34,7 +13,7 @@ typedef struct ListOfMoves{
 		\param board Ponteiro para um tabuleiro.
 		\return Retorna uma lista com os movimentos possíveis de acordo com as peças pretas.
 */
-ListOfMoves* AnalyzePossibleMovementsBlack(TBoard *board);
+//ListOfMoves* AnalyzePossibleMovementsBlack(TBoard *board);
 
 /*Analisa os movimentos possíveis em um tabuleiro em relação as peças brancas.*/
 /*! \fn ListOfMoves AnalyzePossibleMovementsWhite(TBoard *board)
@@ -44,15 +23,6 @@ ListOfMoves* AnalyzePossibleMovementsBlack(TBoard *board);
 		\return Retorna uma lista com os movimentos possíveis de acordo com as peças brancas.
 */
 ListOfMoves* AnalyzePossibleMovementsWhite(TBoard *board);
-
-/*Libera memória dedicada a lista dos movimentos possíveis.*/
-/*! \fn int DeleteMoveList(ListOfMoves* list)
-		\brief Libera memória utilizada por uma lista.
-
-		\param list uma lista de movimentos.
-		\return Retorna um inteiro indicando a falha ou sucesso da operação.
-*/
-int DeleteMoveList(ListOfMoves* list);
 
 /* \fn int verify_syntax_move(char chess_move[])
        \brief Verifica a validade da sintaxe da notação algébrica de um
