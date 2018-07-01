2e14fd5cda115861ab185fa49b76c2d592a0d9c4
daniloinacio <daniloinacio@aluno.unb.br>
2018-07-01 00:05:46 -0300

    função InsertMove teste 1

--- src/list_of_moves.c
+++ src/list_of_moves.c
@@ -3,6 +3,29 @@
 #include <stdlib.h>
 
 
+/*
+  Função: Criar uma lista de movimentos vazia
+        Objetivo:
+            Alocar espaço na memória para uma lista de movimentos.
+
+        Parâmetros:
+            A função não possui parâmetros.
+
+        Retorno:
+        	A  função retorna uma lista de movimentos vazia.
+*/
+
 ListOfMoves* CreateListOfMoves(void){
-	return NULL;
+	ListOfMoves* List = (ListOfMoves*)malloc(sizeof(ListOfMoves));
+
+	List->howmany = 0;
+	List->first = NULL;
+	List->current = NULL;
+	List->last = NULL;
+	
+	return List;
+}
+
+int InsertMove(ListOfMoves* list, Move* play){
+	
 }
