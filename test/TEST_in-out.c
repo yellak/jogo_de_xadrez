9946f37115206d96ab02a07844e3b739054a78cc
Andre Macedo <andremacedo.pv@gmail.com>
2018-07-02 12:48:13 -0300

    Segundo teste para a função de adicionar movimento na lista

--- test/TEST_in-out.c
+++ test/TEST_in-out.c
@@ -19,14 +19,12 @@ TEST(Test_Verify_Start_List, Verify_Funcionality){
 	EXPECT_EQ(list->head, list->last);
 }
 
-/* Teste para verificar a alocação de uma lista de movimentos passados
+/* Teste para verificar a função de adicionar um item para entradas inválidas.
    Procedimento:
-   	-Chamar função de alocar nova lista.
+   	-Chamar função para uma lista nula;
+   	-Chamar função para uma string nula.
    Resultados:
-   	-É esperado que a função retorne um ponteiro não-nulo;
-   	-É esperado que a cabeça não seja nula;
-   	-É esperado que a cabeça aponte para nulo;
-   	-É esperado que o último elemento aponte para a cabeça.
+   	-É esperado que em ambos os testes a função retorne 1.
  */
 TEST(Test_Verify_Add_Move, Verify_Invalid_Entries){
 	char move[] = "Nb1-c3";
@@ -35,6 +33,35 @@ TEST(Test_Verify_Add_Move, Verify_Invalid_Entries){
 	ListPastMoves *list = StartListPM();
 	EXPECT_EQ(1, AddListPM(list, NULL));
 
+	free(list->head);
+	free(list);
+}
+
+/* Teste para verificar a função de adicionar um item
+   Procedimento:
+   	-Chamar a função duas vezes para uma lista criada.
+   Resultados:
+   	-Função deve retornar 0;
+   	-Segundo elemento deve conter a string mandada para ela;
+   	-Último elemento da lista deve conter a string mandada para ela;
+   	-Último elemento da lista deve apontar para NULL;
+   	-Segundo elemento deve apontar para o último.
+ */
+TEST(Test_Verify_Add_Move, Verify_Invalid_Entries){
+	char move1[] = "Nb1-c3";
+	char move2[] = "Pb1-c4";
+	ListPastMoves *list = StartListPM();
+
+	EXPECT_EQ(0, AddListPM(list, move1));
+	EXPECT_EQ(0, AddListPM(list, move2));
+
+	EXPECT_EQ(list->head->next->move, move1);
+	EXPECT_EQ(list->last->move, move2);
+	EXPECT_EQ(list->last, NULL);
+	EXPECT_EQ(list->head->next, list->last);
+
+	free(list->last);
+	free(list->head->next);
 	free(list->head);
 	free(list);
 }
