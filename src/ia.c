fd70b54e28061a42a9b128c30314292a4605f38b
joaopaulom <jpmdoliveira@hotmail.com>
2018-07-03 15:42:03 -0300

    setando as funções iniciais e seus parâmetros

---
+++ src/ia.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include"arv_decisoes.h"
+#include"ia.h"
+
+/* Função: Create_BestTree
+		Objetivo: Criar uma árvore de decisões para ser base da IA
+
+		Parametros:
+			board - Ponteiro para a estrutura do tabuleiro.
+					Não deve ser nulo
+			move - jogada executada pelo jogador.
+			weight - peso do tabuleiro.
+
+		Saída: Essa função retorna um elemento do tipo árvore, contendo as jogadas possíveis para 
+		um movimento específico no tabuleiro.
+*/
+Tree* Create_BestTree(TBoard *board, int weight, Move* play){}
+//função que ordena jogadas, baseadas no peso do tabuleiro
+int Sort_Tree(Tree* tree) {}
+
+//função para extrair melhor jogada , retorna a jogad
+
+//função para retornar as n melhores jogadas
