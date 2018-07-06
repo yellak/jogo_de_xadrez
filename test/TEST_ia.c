97fea0686fbb2b4a00ec02323c0222e1b0721c1f
Felipe Lima <felipesoueu@live.com>
2018-07-06 18:21:58 -0300

    Novo teste para a função de criar a árvore

--- test/TEST_ia.c
+++ test/TEST_ia.c
@@ -15,7 +15,7 @@ TEST(Test_CreateTree, Verify_Creation_Tree){
 	TBoard board;
 	StartStandardBoard(&board);
 	int turn = WHITES_TURN;
-	tree = Create_BestTree(&board, turn);
+	tree = Create_MovesTree(&board, turn);
 
 	/* Testar a alocação */
 	EXPECT_EQ(1, tree != NULL);
@@ -36,10 +36,46 @@ TEST(Test_CreateTree, Verify_Alocation_Tree){
 	Tree * tree;
 	TBoard* board = NULL;
 	int turn = WHITES_TURN;
-	tree = Create_BestTree(board, turn);
+	tree = Create_MovesTree(board, turn);
 
 	/* Testar a alocação */
 	EXPECT_EQ(NULL, tree);
+}
+
+/* Teste para verificar a validade da função que cria a árvore a partir de um tabuleiro
+   Procedimento:
+   	-Criar Variável para a árvore;
+   	-Inicializar um tabuleiro
+   	-Chamar a função que cria a árvore
+   Resultados:
+   	-A árvore alocada deve ser diferente de nula
+   	-A raiz deve ter 20 filhos(número de possibilidades de movimentos)
+   	-Os tabuleiros de todos os nós devem ter peso 0
+ */
+
+TEST(Test_CreateTree, Verify_Root_Information){
+	Tree * tree;
+	TBoard board;
+	StartStandardBoard(&board);
+	int turn = WHITES_TURN;
+	tree = Create_MovesTree(&board, turn);
+
+	/* Testar a alocação */
+	EXPECT_EQ(1, tree != NULL);
+
+	/* Testar se a raiz tem 20 filhos */
+	EXPECT_EQ(20, tree->root->n_child);
+
+	/* Testar se o peso do tabuleiro e de todos os filhos desse nó tem peso 0 */
+	/* pois não há chances de uma peça ser capturada nessa rodada */
+	EXPECT_EQ(0, tree->root->board->Weight);
+
+	for(int i = 0; i < tree->root->n_child; i++){
+		EXPECT_EQ(0, tree->root->child[i]->board->Weight);
+	}
+	
+	FreeTreeNodes(tree->root);
+	free(tree);
 }
 
 int main(int argc, char **argv){
